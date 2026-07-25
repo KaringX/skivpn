b098aabebf3abfa19441ff0480583d967edc2558
GooRingX <gooringx@gmail.com>
2026-07-25 15:13:26 +0800

    update

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -23,12 +23,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance,
                       _In_ wchar_t* command_line,
                       _In_ int show_command) {
   const wchar_t* kWindowName = L"SkiVPN";
-  HWND hwnd = ::FindWindow(L"FLUTTER_RUNNER_WIN32_WINDOW", kWindowName);
-  if (hwnd != NULL) {
-    ::ShowWindow(hwnd, SW_NORMAL);
-    ::SetForegroundWindow(hwnd);
-    return EXIT_FAILURE;
-  }
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
