49e6684db8e7c5c72b284792b895fa1b87439645
GooRingX <gooringx@gmail.com>
2026-08-26 11:17:03 +0800

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -80,6 +80,14 @@ android {
     }
 }
 
+androidComponents {
+    onVariants { variant ->
+        if (variant.buildType == "release") {
+            variant.packaging.jniLibs.excludes.add("**/x86_64/**")
+        }
+    }
+}
+
 flutter { source = "../.." }
 
 dependencies {
